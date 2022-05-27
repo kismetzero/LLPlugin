769ca26db87f4c38bacc8d3f9ea537faba09ead4
KismetNB-Win10 <1500632363@qq.com>
2022-05-27 22:16:09 +0800

    [update] update opTools'opEn by:win10NB

--- src/opTools.js
+++ src/opTools.js
@@ -19,28 +19,27 @@ let confStrTmp = String.raw`{
 
 let conf = data.openConfig(".//plugins//Kismet//config//opTools.json", "json", confStrTmp)
 
-function getPlayerGameMode(player) {
-    let displayMode = player.gameMode;
-    if (displayMode == 0) {
-        let abil = player.getAbilities();
-        if (abil.instabuild == 0 && abil.invulnerable == 0 && abil.mayfly == 1) {
-            return 6;
-        } else { return displayMode; }
-    }
-    return displayMode;
-}
-
-
-function setPlayerFlying(player, bool = false) {
-    let num = Number(bool), nbt = player.getNbt(),
-        abilTag = nbt.getTag("abilities");
-    abilTag.setByte("flying", num);
-    nbt.setTag("abilities", abilTag);
-    return player.setNbt(nbt);
-}
-
 
 if (conf.get("gm")) {
+    function getPlayerGameMode(player) {
+        let displayMode = player.gameMode;
+        if (displayMode == 0) {
+            let abil = player.getAbilities();
+            if (abil.instabuild == 0 && abil.invulnerable == 0 && abil.mayfly == 1) {
+                return 6;
+            } else { return displayMode; }
+        }
+        return displayMode;
+    }
+
+    function setPlayerFlying(player, bool = false) {
+        let num = Number(bool), nbt = player.getNbt(),
+            abilTag = nbt.getTag("abilities");
+        abilTag.setByte("flying", num);
+        nbt.setTag("abilities", abilTag);
+        return player.setNbt(nbt);
+    }
+
     let GAMEMODE = {
         "§3生存模式": 0,
         "§a创造模式": 1,
@@ -155,7 +154,8 @@ if (conf.get("fix")) {
 
 
 if (conf.get("opEn")) {
-    mc.regPlayerCmd('open', 'OP附魔', function (player, args) {
+
+    mc.regPlayerCmd('open all', '快捷通用附魔', function (player, args) {
         if (args.length != 0) {
             ST(player, "§c命令异常,请检测后重试!");
             return;
@@ -189,6 +189,42 @@ if (conf.get("opEn")) {
         mc.runcmd('/enchant ' + pName + ' 17 3');
         ST(player, "§b附魔结束");
     }, 1);
+
+    let dataEnchStrTmp = String.raw`{
+        "tou": "[{"id":26s,"lvl":1s},{"id":17s,"lvl":3s},{"id":3s,"lvl":4s},{"id":1s,"lvl":4s},{"id":4s,"lvl":4s},{"id":0s,"lvl":4s},{"id":8s,"lvl":1s},{"id":6s,"lvl":3s}]"
+    }`;
+
+    let dataEnch = data.openConfig(".//plugins//Kismet//data//enchant.json", "json", dataEnchStrTmp)
+
+    mc.regPlayerCmd("open tou", "附魔你手中的头盔", (player, args) => {
+        if (args.length != 0) {
+            ST(player, "§c命令异常,请检测后重试!");
+            return;
+        }
+        let handIt = player.getHand();
+        if (handIt.isNull()) {
+            ST(player, "§b兄弟,想啥呢,你手里没有物品");
+            return;
+        }
+        let Nbt = handIt.getNbt(), tag = Nbt.getTag("tag"), ench = tag.getTag("ench");
+        if (tag != null && tag.getData("Damage") != 0 && ench != null) {
+            ench = Nbt.parseSNBT(dataEnch.get("tou"));
+            tag.setTag("ench",ench);
+            tag.setInt("Damage", 0);
+            Nbt.setTag("tag", tag);
+            handIt.setNbt(Nbt);
+            let xuid = player.xuid;
+            setTimeout(() => {
+                let pl1 = mc.getPlayer(xuid);
+                if (pl1 != null) {
+                    pl1.refreshItems();
+                }
+            }, 50);//next tick
+            ST(player, "§b附魔完成");
+        } else {
+            ST(player, "§c你手上的物品不需要附魔!");
+        }
+    }, 1);
 }
 
 mc.regPlayerCmd('getinfoi', '获取物品信息', function (player, args) {
@@ -201,15 +237,10 @@ mc.regPlayerCmd('getinfoi', '获取物品信息', function (player, args) {
         ST(player, "§b兄弟,想啥呢,你手里没有物品");
         return;
     }
-    let Nbt = handIt.getNbt(), tag = Nbt.getTag("tag");
-    if (tag != null ) {
-        let SNBT = tag.toSNBT();
-        let flag = File.writeTo(".//plugins//Kismet//date//opTools.txt", SNBT);
-        if (flag) {
-            ST(player, "写入成功");
-        }
-        ST(player, SNBT);
-        }
+    let Nbt = handIt.getNbt(), tag = Nbt.getTag("tag"), ;
+    if (tag != null && ) {
+        
+    }
 }, 1);
 
 logger.info("快捷工具集已装载!作者:kismet,版本:1.0,代码参考:提米吖");
